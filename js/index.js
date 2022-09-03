3b0e64fac4f324d81fb269b5263620fde2772f41
salmamouri <mouriclub@gmail.com>
2022-09-04 01:39:41 +0600

    updated

--- js/index.js
+++ js/index.js
@@ -77,7 +77,27 @@ const displayNews = (data) => {
       <p> <i class="fa-solid fa-star"></i> ${item.rating.number}</p>
       <p><i class="fa-solid fa-arrow-right"></i></p></div>
       
-      
+      <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Read Details</button>
+
+      <!-- Modal -->
+<div class="modal fade  " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">${item.title}</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <img  class="img-fluid" src="${item.image_url}"/>
+        <p>${item.details}</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
      
     </div>
      
@@ -89,11 +109,11 @@ const displayNews = (data) => {
   toggleSpinner(false);
 };
 
-const displayModal = (id) => {
-  console.log(id);
-  const modalContainer = document.getElementById("modalContainer");
-  const modalDiv = document.createElement("div");
-};
+// const displayModal = (id) => {
+//   console.log(id);
+//   const modalContainer = document.getElementById("modalContainer");
+//   const modalDiv = document.createElement("div");
+// };
 
 // const modalContent = (id) => {
 //   const url = `https://openapi.programming-hero.com/api/news/category/0${id}`;
